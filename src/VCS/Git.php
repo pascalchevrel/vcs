d012a572571544e9a3f8cf1c74933fe6c8ebd495
Pascal Chevrel <pascal.chevrel@free.fr>
2015-10-06 10:27:23 +0200

    Fix git log to work on subdirectories, like svn

--- src/VCS/Git.php
+++ src/VCS/Git.php
@@ -11,7 +11,8 @@ class Git extends Base
     public function getCommits()
     {
         $log = $this->execute(
-            "git log --no-merges --format='changeset: %H%nuser: %aN <%aE>%ndate: %ad%nsummary: %s%n'"
+            "git log --no-merges --format='changeset: %H%nuser: %aN <%aE>%ndate: %ad%nsummary: %s%n' "
+            . $this->repository_path
         );
         $this->repository_type = 'git';
 
