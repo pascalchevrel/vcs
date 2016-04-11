a42962bce443156ba073ce1cad5472fe866c41d0
Théo Chevalier <theo.chevalier11@gmail.com>
2016-04-11 15:20:03 +0200

    Add a function to get file changes for a given revision (#2)
    * Add a function to get file changes for a given revision
    
    * typofix
    
    * Adding revertFile()

--- src/VCS/Mercurial.php
+++ src/VCS/Mercurial.php
@@ -17,6 +17,28 @@ class Mercurial extends Base
     }
 
     /**
+     * Get the list of files changed with the type of change for a given revision.
+     * Results are returned as a structured array.
+     *
+     * @param String $rev A valid changeset for this repo
+     * @return array List of file changes
+     */
+    public function getChangedFiles($rev)
+    {
+        $raw_changes = $this->execute('hg status --change ' . $rev);
+        $this->repository_type = 'hg';
+
+        $changes = [];
+        foreach ($raw_changes as $key => $change) {
+            $exploded_change = explode(' ', $change);
+            $changes[$key]['type'] = $exploded_change[0];
+            $changes[$key]['path'] = $exploded_change[1];
+        }
+
+        return $changes;
+    }
+
+    /**
      * Do a "hg pull -r default ; hg update -C" command
      * @return void
      */
@@ -24,5 +46,16 @@ class Mercurial extends Base
     {
         $this->execute('hg pull -r default');
         $this->execute('hg update -C');
+    }
+
+    /**
+     * Revert a file to a particular revision.
+     *
+     * @param String $rev A valid changeset for this repo
+     * @param String $path The path of a file
+     * @return void
+     */
+    public function revertFile($rev, $path) {
+        $this->execute("hg revert -r {$rev} {$path}");
     }
 }
